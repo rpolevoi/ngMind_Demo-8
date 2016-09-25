a48c0fdc96be4dcdbd27a5d6e3ffca70ceb11b5d
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-25 12:46:25 -0700

    Create child.component.ts

---
+++ app/child/child.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-child-component',
+  template: `<h1>This text is in child component</h1>`,
+  styleUrls: ['child.component.css']
+})
+export class ChildComponent {}
